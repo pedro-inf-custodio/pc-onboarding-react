6476fb9c235b7d5d05f4dc567a60cbdee0c42300
Pedro Custodio <p.custodio@runtime-revolution.com>
2022-11-08 15:44:43 +0000

    Implementation Home and Search Page

--- src/App.js
+++ src/App.js
@@ -1,10 +1,23 @@
+import React, { useState } from "react";
+import { Route, Routes } from "react-router-dom";
+import Footer from "./components/Footer";
+import SearchBar from "./components/SearchBar";
+import Home from "./pages/Home.js";
+import Search from "./pages/Search.js";
 
-function App() {
+export default function App() {
+  const [search, setSearch] = useState("");
+
   return (
-    <div className='bg-slate-100 h-screen'>
-      <p> Sample </p>
+    <div className="bg-slate-50 h-full font-light">
+      <SearchBar search={search} setSearch={setSearch} />
+
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/results" element={<Search />} />
+      </Routes>
+
+      <Footer />
     </div>
   );
 }
-
-export default App;
---
+++ src/pages/Search.js
@@ -0,0 +1,23 @@
+import React, { useState, useEffect } from "react";
+import { useSearchParams } from "react-router-dom";
+import { MoviesList } from "../components/MoviesList";
+import { fetchMovies } from "../helpers/fetchMovies.js";
+import { URL_API_SEARCH } from "../helpers/constants.js";
+
+export default function Search() {
+  const [movieData, setMovieData] = useState(null);
+  const [searchParams] = useSearchParams();
+  let input = searchParams.get("search");
+
+  useEffect(() => {
+    fetchMovies(URL_API_SEARCH, input).then(({ results }) =>
+      setMovieData({ results })
+    );
+  }, [input]);
+
+  return (
+    <div>
+      {movieData && <MoviesList popularMoviesData={movieData.results} />}
+    </div>
+  );
+}
